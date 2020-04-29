74fcbef5afd62bbbb78cd4dba257014d2626a82b
Sutan Syahdinullah <sutansss20091997@gmail.com>
2020-04-29 21:11:35 +0700

    Menu Functionality and UI Home Fixed

--- app/src/main/java/com/waynestudio/satgaspol/MainActivity.kt
+++ app/src/main/java/com/waynestudio/satgaspol/MainActivity.kt
@@ -2,11 +2,52 @@ package com.waynestudio.satgaspol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private var SELECTED_MENU = "selected_menu"
+    }
+
+    private var navigationItemSelected: BottomNavigationView.OnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener {
+            var fragment: Fragment? = null
+            if (it.itemId == R.id.action_home) {
+                fragment = HomeFragment.newInstance()
+            } else if (it.itemId == R.id.action_paket) {
+                fragment = PackageFragment.newInstance()
+            } else if(it.itemId == R.id.action_more){
+                fragment = MoreFragment.newInstance()
+            }
+
+            if (fragment != null) {
+                supportFragmentManager.beginTransaction()
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .replace(R.id.container, fragment)
+                    .commit()
+            }
+
+            true
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nav_view.setOnNavigationItemSelectedListener(navigationItemSelected)
+
+        if (savedInstanceState != null) {
+            savedInstanceState.getInt(SELECTED_MENU)
+        } else {
+            nav_view.selectedItemId = R.id.action_home
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(SELECTED_MENU, nav_view.selectedItemId)
     }
 }
